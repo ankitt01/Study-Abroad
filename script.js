ca4437cd75c845f1a9750ccce7c1b9594537928a
ankitt01 <ankitrawat0102@gmail.com>
2020-12-01 09:00:51 +0530

    navbar done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bar1 = document.querySelector('.bar1')
 const bar2 = document.querySelector('.bar2')
 const bar3 = document.querySelector('.bar3')
 
+
 const navLinks = document.querySelector('.nav-links')
 
 bars.addEventListener('click', function() {
@@ -15,5 +16,6 @@ bars.addEventListener('click', function() {
     bar1.classList.toggle('animate1');
     bar3.classList.toggle('animate2');
 
+    // navLinks.style.display = "block"
     navLinks.classList.toggle('nav-link-active')
 })
