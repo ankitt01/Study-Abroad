4db4294c0aa23643090c57c762e6590d33992467
ankitt01 <ankitrawat0102@gmail.com>
2020-12-07 22:34:41 +0530

    fixed navigation

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const bar1 = document.querySelector('.bar1')
 const bar2 = document.querySelector('.bar2')
 const bar3 = document.querySelector('.bar3')
 
+const links = document.querySelectorAll('.link')
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-link-active')
+    })
+})
 
 const navLinks = document.querySelector('.nav-links')
 
